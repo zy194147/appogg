5920fbcb24a34ed7540d043b7222bec83da05998
zy194147 <zy194147@126.com>
2019-02-02 14:30:55 +0800

    初始化

---
+++ src/test/java/com/appogg/website/WebsiteApplicationTests.java
@@ -0,0 +1,17 @@
+package com.appogg.website;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebsiteApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
